8a1be1dfc40401e93c51df12989907fc8bc5f006
iamzainn <iamzainnf@gmail.com>
2024-01-26 15:28:31 +0500

    Improve Header design using flowbite Perfectly

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -1,53 +1,61 @@
-import { Outlet ,Link,NavLink,useLocation} from "react-router-dom"
-import { Fragment } from "react"
-import { Button, Navbar, TextInput } from "flowbite-react"
-
+import { Outlet, Link, NavLink, useLocation } from "react-router-dom";
+import { Button, Navbar, TextInput } from "flowbite-react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
-
-
 const Header = () => {
   const pth = useLocation().pathname;
   return (
-    <Fragment>
-    <Navbar className="border-b-2">
-      <Link to= "/" className="self-center whitespace-nowrap text-sm sm:text-lg font-semibold dark:text-white">
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white text-center">My</span>
-         blog's
-      </Link>
-      
-      <form className="">
-        <TextInput className="hidden md:inline-block" type="text" placeholder="search.."rightIcon={AiOutlineSearch}
-        ></TextInput>
-      </form>
-      <Button className="sm:hidden border-2 border-blue-200" pill color="grey"><AiOutlineSearch></AiOutlineSearch></Button>
-     <div className="flex gap-2 sm:order-2">
-      <Button className="hidden sm:inline-block" pill color="grey">
-        <FaMoon></FaMoon>
-      </Button>
-      <Button gradientDuoTone="purpleToBlue" outline>
-        <Link to="/Sign-in">Sign In</Link>
-      </Button>
-      <Navbar.Toggle></Navbar.Toggle>
-     </div>
-     <Navbar.Collapse>
-      <Navbar.Link active = {pth=="/"} as={"div"}>
-        <NavLink to="/" >Home</NavLink>
-      </Navbar.Link>
-      <Navbar.Link active = {pth=="/about"} as={"div"}>
-        <NavLink to="/about">About</NavLink>
-      </Navbar.Link>
-      <Navbar.Link active = {pth=="/Projects"} as={"div"}>
-        <NavLink to="/Projects">Projects</NavLink>
-      </Navbar.Link>
-     </Navbar.Collapse>
-    </Navbar>
-    <main>
-        <Outlet></Outlet>
-    </main>
-    </Fragment>
-    
-  )
-}
+    <>
+      <Navbar className="border-b-2 px-4 flex-wrap">
+        <div className="logo flex items-center justify-center whitespace-nowrap sm:text-lg xl:text-2xl font-semibold dark:text-white">
+          <Link to="/">
+            <span className="px-1 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white text-center">
+              My
+            </span>
+            Logo
+          </Link>
+        </div>
+        <div className="search flex gap-2">
+          <Button className="lg:hidden" pill color="light">
+            <AiOutlineSearch className="text-sm"></AiOutlineSearch>
+          </Button>
+          <form className="hidden lg:block">
+            <TextInput
+              type="text"
+              id="search"
+              placeholder="Search Here"
+              rightIcon={AiOutlineSearch}
+            ></TextInput>
+          </form>
+        </div>
 
-export default Header
+        <div className="hamburgerBtns flex gap-2 md:order-5">
+          <Button className="hidden sm:block" pill color="light">
+            <FaMoon className="text-sm"></FaMoon>
+          </Button>
+          <Button outline gradientDuoTone="purpleToPink">
+            <Link to="/Sign-in">Sign in</Link>{" "}
+          </Button>
+          <Navbar.Toggle></Navbar.Toggle>
+        </div>
+
+        <Navbar.Collapse>
+          <Navbar.Link active={pth === "/"} as={"div"}>
+            <NavLink to="/">Home</NavLink>
+          </Navbar.Link>
+          <Navbar.Link active={pth === "/about"} as={"div"}>
+            <NavLink to="/about">About</NavLink>
+          </Navbar.Link>
+          <Navbar.Link active={pth === "/projects"} as={"div"}>
+            <NavLink to="/projects">Projects</NavLink>
+          </Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
+      <main>
+        <Outlet></Outlet>
+      </main>
+    </>
+  );
+};
+
+export default Header;
